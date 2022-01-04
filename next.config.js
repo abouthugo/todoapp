96834ae7e5d8167ce0e634ed23bbe115d088fc8a
abouthugo <hugo@perdomo.dev>
2022-01-04 14:30:38 -0500

    reverting next config file

--- next.config.js
+++ next.config.js
@@ -1,26 +1,5 @@
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-
-module.exports = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        mozjpeg: {
-          quality: 80,
-        },
-        pngquant: {
-          speed: 3,
-          strip: true,
-          verbose: true,
-        },
-        imagesPublicPath: "/todoapp/_next/static/",
-      },
-    ],
-  ],
-  {
-    reactStrictMode: true,
-    basePath: "/todoapp",
-    assetPrefix: "/todoapp/",
-  }
-);
+module.exports = {
+  reactStrictMode: true,
+  basePath: "/todoapp",
+  assetPrefix: "/todoapp/",
+};
