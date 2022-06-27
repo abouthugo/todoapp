97a58cf196ecf6b540c73a2b043d4423c3e67590
abouthugo <35211237+abouthugo@users.noreply.github.com>
2022-06-27 16:18:20 +0000

    Deploying to gh-pages from @ abouthugo/todoapp@ec5e85c03ece3e59417c56ae869c073b456b7fbb 🚀

--- _next/static/Hzlmydu06DPowxFHcV_Bg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8b52ec7acf040656.css","static/chunks/pages/index-4b9ce11c4c9027cf.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
